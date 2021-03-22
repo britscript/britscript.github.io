a7debeb7631df1072744031c7acd4f73528b9a3e
britscript <65099698+britscript@users.noreply.github.com>
2021-03-22 12:07:34 -0700

    initial commit

---
+++ app.js
@@ -0,0 +1,40 @@
+
+// responsive nav bar
+const navSlide = () => {
+    const burger = document.querySelector('.burger');
+    const nav = document.querySelector('.nav-links');
+    const navLinks = document.querySelectorAll('.nav-links li');
+    // toggle
+    burger.addEventListener('click', () => {
+        nav.classList.toggle('nav-active');
+        // animations
+        navLinks.forEach((link, index) => {
+            if (link.style.animation) {
+                link.style.animation = '';
+            } else {
+                link.style.animation = `navLinkFade 0.5s ease forwards ${index / 7 + 0.5}s`;
+            }
+        });
+
+        // burger animation
+        burger.classList.toggle('toggle');
+    });
+
+}
+
+navSlide();
+
+// sticky navbar
+window.onscroll = function() {myFunction()};
+
+var navbar = document.getElementById("navbar");
+
+var sticky = navbar.offsetTop;
+
+function myFunction() {
+  if (window.pageYOffset >= sticky) {
+    navbar.classList.add("sticky")
+  } else {
+    navbar.classList.remove("sticky");
+  }
+}
